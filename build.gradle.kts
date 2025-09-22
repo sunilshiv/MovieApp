e9baf4d9b4d3cf2f20adcc94353d051bca1a41ff
sunilshiv <sunil.d.shiv@gmail.com>
2025-09-22 13:31:50 +0530

    Update gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,8 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    id("com.google.devtools.ksp") version "2.2.10-2.0.2"
 }
