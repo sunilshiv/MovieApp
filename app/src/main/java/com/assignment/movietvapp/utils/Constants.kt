d3a4eadb9b817c3f78f916088dc518a8b05d3d3b
Sunil D Shivanakkanavar <sunil.d.shiv@gmail.com>
2025-09-24 11:01:22 +0530

    Update Constants.kt

--- app/src/main/java/com/assignment/movietvapp/utils/Constants.kt
+++ app/src/main/java/com/assignment/movietvapp/utils/Constants.kt
@@ -5,7 +5,7 @@ class Constants {
     companion object {
 
         const val BASE_URL = "https://api.themoviedb.org/3/"
-        const val API_KEY = "5bdaf4c804d296b88507ebc83fda5334"//put your api key
+        const val API_KEY = ""//put your api key
         const val BASE_BACKDROP_IMAGE_URL = "https://image.tmdb.org/t/p/w780/"
         const val BASE_POSTER_IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
 
